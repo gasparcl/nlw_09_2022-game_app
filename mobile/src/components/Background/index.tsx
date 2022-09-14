98c95d2d0418d5f77cc5f1d4f3d4bbaea9ddbdcb
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-09-14 14:00:38 -0300

    att arquivos aula 2-6 | high speed

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+import { ImageBackground } from "react-native"
+
+import backgroundImg from "../../assets/background-galaxy.png"
+
+import { styles } from "./styles"
+
+interface Props {
+    children: React.ReactNode
+}
+
+export function Background({ children }: Props) {
+    return (
+        <ImageBackground
+            source={backgroundImg}
+            defaultSource={backgroundImg} // memoriza a img padrão e evita delays de carregamento
+            style={styles.container}
+        >
+            {children}
+        </ImageBackground>
+    )
+}
